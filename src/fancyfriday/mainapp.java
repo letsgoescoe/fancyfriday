62913d9936b24f5ac2effa0f3bcf1a72ef5817af
Dominique Escoe <descoe2001@yahoo.com git config --global user.email descoe2001@yahoo.com>
2016-09-08 11:23:10 -0400

    initial commit

---
+++ src/fancyfriday/mainapp.java
@@ -0,0 +1,20 @@
+/**
+ * This is a demo app 
+ */
+package fancyfriday;
+
+/**
+ * @author Student
+ *
+ */
+public class mainapp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
